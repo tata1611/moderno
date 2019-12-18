6bac78095885ad61778494601f1021b2c4f6dd3e
Tata1611 <ooorubiconch@gmail.com>
2019-12-18 20:19:09 +0300

    end

--- app/js/main.js
+++ app/js/main.js
@@ -9,9 +9,37 @@ $(function(){
         dots:true,
         arrows:false,
         slidesToShow:4,
-        slidesToScroll:4
-
-    });
+        slidesToScroll:4,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+         ]
+});
 
     $(".js-range-slider").ionRangeSlider({
         type: "double",
